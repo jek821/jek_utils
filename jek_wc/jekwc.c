991bf7c40802782fbf7a7f233b0dd526c43e6f88
jemanuel <jacobkriss2001@gmail.com>
2026-06-14 02:00:07 -0400

    updated wc

--- jek_wc/jekwc.c
+++ jek_wc/jekwc.c
@@ -1,11 +1,44 @@
 #include <stdio.h>
+#include <unistd.h>
+
+extern int optind;
+
+typedef struct
+{
+    int show_bytes;
+    int show_lines;
+    int show_words;
+
+} Options;
+
+int handle_flags(const int argc, char *argv[], Options *flags)
+{
+    int opt;
+    while ((opt = getopt(argc, argv, "blw")) != -1)
+    {
+        switch (opt)
+        {
+        case 'b':
+            flags->show_bytes = 1;
+            break;
+        case 'l':
+            flags->show_lines = 1;
+            break;
+        case 'w':
+            flags->show_words = 1;
+            break;
+        case '?':
+            return -1;
+
+        default:
+        }
+    }
+    return optind - 1;
+}
 
 int main(int argc, char *argv[])
 {
-    for (int i = 0; i < argc; i++)
-    {
-        fprintf(stdout, "Arg %d: %s", i, argv[1]);
-    }
-
-    return 0;
+    Options flags;
+    int optind = handle_flags(argc, argv, &flags);
+    printf("%d", optind);
 }
